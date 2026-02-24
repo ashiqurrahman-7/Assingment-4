2d56b345e225bcb39eeaaee9675e29db0664f4c5
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-02-24 15:30:34 +0600

    here i create interview rendering where i create a div and inside it i add a innerHTML and then append them to the filter section

--- script.js
+++ script.js
@@ -129,3 +129,55 @@ mainContainer.addEventListener('click', function(event){
         calculateCount();
     }
 })
+
+
+
+
+
+
+//This is our Rendering phase 
+//rendering Interview
+function renderInterview(){
+    filterSection.innerHTML = '';
+   
+    if(interviewList.length === 0){
+        emptyState.classList.remove('hidden');
+        filterSection.appendChild(emptyState);
+        return;
+    } else {
+        emptyState.classList.add('hidden');
+    }
+    
+
+    interviewList.forEach(interview => {
+
+        let div = document.createElement('div');
+        div.className = 'space-y-6  border-gray-400 shadow-xl hover:shadow-2xl transition duration-300 rounded-2xl  mb-6';
+
+        div.innerHTML = `
+          
+            <div class="space-y-6 p-8">
+                    <div class="flex justify-between">
+                        <div class="space-y-2">
+                        <p class="text-2xl companyName ">${interview.companyName}</p>
+                        <p class="position">${interview.position}</p>
+                    </div> 
+                    <div class="delete-card-btn"><i class="fa-solid fa-trash"></i></div>
+                    </div>
+                    <div class="flex gap-3 ">
+                        <p class="address">${interview.address}</p>
+                        <p class="jobSchedule">${interview.jobSchedule}</p>
+                        <p class="salary">${interview.salary}</p>
+                    </div>
+                    <p class="status">${interview.status}</p>
+                    <p class="notes">${interview.notes}</p>
+                   <button class="interview-btn border border-green-400 text-green-400 font-bold px-4 py-2 hover:bg-green-400 hover:text-white transition duration-300 rounded">INTERVIEW</button>
+
+                         <button class="rejected-btn border border-red-400 text-red-400 font-bold px-4 py-2 hover:bg-red-400 hover:text-white transition duration-300"> REJECTED</button>
+                    
+                </div>
+        `;
+
+        filterSection.appendChild(div);
+    });
+}
