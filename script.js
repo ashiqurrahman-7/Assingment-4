4fe6ff9d57ae17eb358e81d1029a56648fe3ddc2
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-02-24 15:21:19 +0600

    This is our delegation phase here we collect information of our card and can changes status when click in interview or review button.But still this card wont go to interviewList or rejectedList.After this section we add rendering we try to implement it on that section

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let interviewList = [];
+let rejectedList = [];
+let currentStatus = 'all';
+
 let total = document.getElementById("total");
 let interviewCount = document.getElementById("interviewCount");
 let rejectedCount = document.getElementById("rejectedCount");
@@ -39,3 +43,89 @@ function toggleStyle(id){
     selected.classList.remove('bg-gray-300','text-black');
     selected.classList.add('bg-blue-500','text-white');
 }
+
+
+
+
+
+//This is our main container (Delegation phase)section here we collect our card information and make object of the data  and push them in the interviewList or rejectedList
+mainContainer.addEventListener('click', function(event){
+
+        const parentNode = event.target.closest('.space-y-6');
+    if (!parentNode) return;
+
+    if(event.target.classList.contains('interview-btn')){
+
+        const companyName = parentNode.querySelector('.companyName').innerText;
+        const position = parentNode.querySelector('.position').innerText;
+        const address = parentNode.querySelector('.address').innerText;
+        const jobSchedule = parentNode.querySelector('.jobSchedule').innerText;
+        const salary = parentNode.querySelector('.salary').innerText;
+        const notes = parentNode.querySelector('.notes').innerText;
+
+        parentNode.querySelector('.status').innerText = 'INTERVIEWED';
+         
+        updateAllSectionStatus(companyName, 'INTERVIEWED');
+        
+
+        const cardInfo = {
+            companyName,
+            position,
+            address,
+            jobSchedule,
+            salary,
+            status: 'INTERVIEWED',
+            notes
+        };
+
+          if (!interviewList.find(item => item.companyName === companyName)) {
+            interviewList.push(cardInfo);
+        }
+
+        rejectedList = rejectedList.filter(item => item.companyName !== companyName);
+
+        if(currentStatus === 'rejected-filter-btn'){
+            renderRejected();
+        }
+
+        calculateCount();
+
+}
+
+        if(event.target.classList.contains('rejected-btn')){
+
+        const companyName = parentNode.querySelector('.companyName').innerText;
+        const position = parentNode.querySelector('.position').innerText;
+        const address = parentNode.querySelector('.address').innerText;
+        const jobSchedule = parentNode.querySelector('.jobSchedule').innerText;
+        const salary = parentNode.querySelector('.salary').innerText;
+        const notes = parentNode.querySelector('.notes').innerText;
+
+        parentNode.querySelector('.status').innerText = 'REJECTED';
+        
+        updateAllSectionStatus(companyName, 'REJECTED');
+        
+
+        const cardInfo = {
+            companyName,
+            position,
+            address,
+            jobSchedule,
+            salary,
+            status: 'REJECTED',
+            notes
+        };
+
+        if (!rejectedList.find(item => item.companyName === companyName)) {
+            rejectedList.push(cardInfo);
+        }
+
+        interviewList = interviewList.filter(item => item.companyName !== companyName);
+
+        if(currentStatus === 'interview-filter-btn'){
+            renderInterview();
+        }
+
+        calculateCount();
+    }
+})
